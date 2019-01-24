e62f0203cf637bb750fcaecbc2e995af7456fc8b
sidharth-jamuar <rahuljamuar21@gmail.com>
2019-01-25 02:12:21 +0530

    private messaging

--- server/index.js
+++ server/index.js
@@ -1,28 +1,41 @@
 const app=require("express")();
 const server=require("http").createServer(app)
 const io=require("socket.io").listen(server);
-var nicknames=[];
+var users={};
 const PORT=process.env.PORT || 3004;
 
 require("./routes/chatRoutes")(app)
 
 io.sockets.on('connection',socket=>{
     socket.on('send message',data=>{
-        
-        io.sockets.emit('new-message',{msg:data,nick:socket.nickname});
+        let msg=data.trim();
+        if(msg.substr(0,3)==="/w "){
+            console.log("private")
+            msg=msg.substr(3);
+            var index=msg.indexOf(' ')
+            var name=msg.substring(0,index);
+            mainmsg=msg.substring(index+1)
+            if(name in users){
+                socket.emit("whisper",{msg:msg,nick:socket.nickname})
+                users[name].emit("whisper",{msg:msg,nick:socket.nickname})
+            }
+        }
+        else{
+        io.sockets.emit('new-message',{msg:msg,nick:socket.nickname});
+        }
     })
     function updateNicknames(){
-        io.sockets.emit('user list',nicknames);
+        io.sockets.emit('user list',Object.keys(users));
     }
     socket.on('new user',(data,callback)=>{
         
-        if(nicknames.indexOf(data)!== -1){
+        if(data in users){
             callback(false)
         }
         else{
             callback(true);
             socket.nickname=data;
-            nicknames.push(socket.nickname);
+            users[socket.nickname]=socket
             updateNicknames();
             
         }
@@ -33,7 +46,7 @@ io.sockets.on('connection',socket=>{
         }
         else{
            
-           nicknames.splice(nicknames.indexOf(socket.nickname),1)
+           delete users[socket.nickname]
            updateNicknames();
         }
     })
